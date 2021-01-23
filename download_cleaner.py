0db465f89b80a1b933e41e6957dc66df875d3419
Ítalo <67923640+italopinto@users.noreply.github.com>
2021-01-23 18:00:10 -0300

    Update download_cleaner.py

--- download_cleaner.py
+++ download_cleaner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import time
 import logging
@@ -16,46 +18,55 @@ class Handler(FileSystemEventHandler):
     def on_modified(event):
         if os.path.isdir(event.src_path):
             return
-        if is_code_file(event) == True:
+        if is_code_file(event):
             path_to_folder = make_folder('code')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_text_file(event) == True:
+        elif is_text_file(event):
             path_to_folder = make_folder('text')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_pdf_file(event) == True:
+        elif is_pdf_file(event):
             path_to_folder = make_folder('pdf')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_mp3_file(event) == True:
+        elif is_mp3_file(event):
             path_to_folder = make_folder('audio')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_image_file(event) == True:
+        elif is_image_file(event):
             path_to_folder = make_folder('images')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_video_file(event) == True:
-            path_to_folder = make_folder('videos')
+        elif is_video_file(event):
+            try:
+                # if the video is larger than 50 mb
+                if os.path.getsize(str(event.src_path)) > 50000000:
+                    return logging.info('File is too large!')
+                else:
+                    path_to_folder = make_folder('videos')
+                    move_to_new_corresponding_folder(event, path_to_folder)
+                    return
+            except FileNotFoundError:
+                pass
+        elif is_doc_file(event):
+            path_to_folder = make_folder('documents')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_doc_file(event) == True:
-            path_to_folder = make_folder('word documents')
-            move_to_new_corresponding_folder(event, path_to_folder)
-            return
-        elif is_spreadsheet_file(event) == True:
+        elif is_spreadsheet_file(event):
             path_to_folder = make_folder('spreadsheets')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_presentation_file(event) == True:
+        elif is_presentation_file(event):
             path_to_folder = make_folder('presentation files')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
-        elif is_executable_file(event) == True:
+        elif is_executable_file(event):
             path_to_folder = make_folder('executable files')
             move_to_new_corresponding_folder(event, path_to_folder)
             return
+        elif is_zip_file(event):
+            return logging.info(f'{str(event.src_path)} is a compacted file, do nothing')
 
     @staticmethod
     def on_deleted(event):
@@ -66,11 +77,18 @@ class Handler(FileSystemEventHandler):
         pass
 
 
+logging.basicConfig(level=logging.INFO,
+                    filename='folder_watcher.log',
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S')
 file_change_handler = Handler()
+event_handler = LoggingEventHandler()
 observer = Observer()
-os.chdir('E:\\Storage\\Download')
-print(os.getcwd())
-observer.schedule(file_change_handler, os.getcwd(), recursive=False,)
+os.chdir('/home/italo/Downloads')
+pwd = os.getcwd()
+logging.info('Watchdog is watching: ' + pwd)
+observer.schedule(file_change_handler, pwd, recursive=False)
+observer.schedule(event_handler, pwd, recursive=True)
 observer.start()
 
 try:
