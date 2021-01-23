fb1be33ddb4be6a92fdd3c273909668734174ac7
Ítalo <67923640+italopinto@users.noreply.github.com>
2021-01-23 18:03:01 -0300

    Update file_utilities.py

--- file_utilities.py
+++ file_utilities.py
@@ -1,9 +1,13 @@
+import logging
 import shutil
 import os
 
 
 def extension_type(event):
-    return event.src_path[event.src_path.rindex('.') + 1:]
+    try:
+        return event.src_path[event.src_path.rindex('.') + 1:]
+    except:
+        return logging.info(f'File {str(event.src_path)} doesn\'t have extension!')
 
 
 def is_text_file(event):
@@ -25,19 +29,19 @@ def is_mp3_file(event):
 
 
 def is_image_file(event):
-    if extension_type(event) in ('png', 'jpg', 'bmp', 'gif', 'raw'):
+    if extension_type(event) in ('png', 'jpg', 'jpeg', 'bmp', 'gif', 'raw', 'svg'):
         return True
     return False
 
 
 def is_video_file(event):
-    if extension_type(event) in ('mov', 'mp4', 'avi', 'flv'):
+    if extension_type(event) in ('mp4', 'avi', 'mvk'):
         return True
     return False
 
 
 def is_doc_file(event):
-    if extension_type(event) in ('doc', 'docx'):
+    if extension_type(event) in ('doc', 'docx', 'odt'):
         return True
     return False
 
@@ -49,13 +53,13 @@ def is_spreadsheet_file(event):
 
 
 def is_presentation_file(event):
-    if extension_type(event) in ('ppt', 'pptx'):
+    if extension_type(event) in ('ppt', 'pptx', 'odp'):
         return True
     return False
 
 
 def is_code_file(event):
-    if extension_type(event) in ('py', 'cs', 'js', 'php', 'html', 'sql', 'css'):
+    if extension_type(event) in ('py', 'cs', 'js', 'php', 'html', 'sql', 'css', 'c'):
         return True
     return False
 
@@ -66,10 +70,16 @@ def is_executable_file(event):
     return False
 
 
+def is_zip_file(event):
+    if extension_type(event) in ('zip', 'gz'):
+        return True
+    return False
+
+
 def make_folder(foldername):
-    os.chdir('E:\\Storage\\Download')
-    if os.path.exists(foldername) == True:
-        print('Folder already exists, skipping creation')
+    # os.chdir('/home/italo/Downloads')
+    if os.path.exists(foldername):
+        logging.info('Folder already exists, skipping creation')
         return os.getcwd() + os.sep + str(foldername)
     else:
         os.mkdir(str(foldername))
@@ -79,7 +89,7 @@ def make_folder(foldername):
 def move_to_new_corresponding_folder(event, path_to_new_folder):
     try:
         shutil.move(event.src_path, path_to_new_folder)
-        print('moving file')
+        logging.info('moving file')
     except:
-        print('File already existed in target folder')
+        logging.info('File already existed in target folder')
         pass
